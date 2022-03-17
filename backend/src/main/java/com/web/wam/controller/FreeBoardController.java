ce6c8ea7c6117a2088a721ee77a4f0860ed88c87
minjunglee <3sally@naver.com>
2022-03-17 16:01:39 +0900

    :fire: Feat : Add BaseResponse BaseResponse 서버 요청에 대한 기본 응답객체

--- backend/src/main/java/com/web/wam/controller/FreeBoardController.java
+++ backend/src/main/java/com/web/wam/controller/FreeBoardController.java
@@ -1,11 +1,40 @@
 package com.web.wam.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web.wam.model.dto.BaseResponse;
+import com.web.wam.model.service.FreeBoardService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @Api(value = "자유게시판 API", tags = { "freeboard" })
-@Controller
+@RestController
+@RequestMapping("/freeboard")
 public class FreeBoardController {
-
+	@Autowired
+	FreeBoardService freeBoardService;
+	
+//	@GetMapping
+//	@ApiOperation(value = "게시글 리스트", notes = "자유게시판 전체 글 불러오기")
+//	@ApiResponses({ @ApiResponse(code = 200, message = "성공", response = BaseResponseBody.class),
+//			@ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
+//			@ApiResponse(code = 404, message = "보드 없음", response = BaseResponseBody.class),
+//			@ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class) })
+//
+//	public ResponseEntity<? extends BaseResponseBody> create(
+//			@RequestBody @ApiParam(value = "보드 생성 정보", required = true) BoardCreatePostReq boardCreateInfo) {
+//
+//		freeBoardService.createBoard(boardCreateInfo);
+//		return ResponseEntity.status(200).body(BoardCreatePostRes.of(200, "Success", boardRandom));
+//	}
 }
