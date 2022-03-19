fa6f5b85b4c61d0dd05534f1114aed3cbf2b27e4
정원식 <wonsic3686@gmail.com>
2022-03-20 03:19:26 +0900

    🔥 Feat: Add AlreadyExistEmailException

---
+++ backend/src/main/java/com/web/wam/exception/AlreadyExistEmailException.java
@@ -0,0 +1,7 @@
+package com.web.wam.exception;
+
+public class AlreadyExistEmailException extends RuntimeException{
+
+    private static final String MESSAGE = "이미 등록된 이메일입니다.";
+    public AlreadyExistEmailException() { super(MESSAGE); }
+}
