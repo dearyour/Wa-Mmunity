43bd5a1c440e7baf4d920291d9e121045b1773ed
정원식 <wonsic3686@gmail.com>
2022-04-02 22:28:32 +0900

    Hotfix: cors issue
    Former-commit-id: eb26712b3490f8a3359aabd10811e9651d37a290

--- backend/src/main/java/com/web/wam/config/security/WebSecurityConfig.java
+++ backend/src/main/java/com/web/wam/config/security/WebSecurityConfig.java
@@ -13,6 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -34,8 +37,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().disable().httpBasic().disable() // rest api 만을 고려하여 기본 설정은 해제하겠습니다.
-				.csrf().disable() // csrf 보안 토큰 disable처리.
+
+		http.httpBasic().disable().cors().configurationSource(corsConfig()) // rest api 만을 고려하여 기본 설정은 해제하겠습니다.
+				.and().csrf().disable() // csrf 보안 토큰 disable처리.
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 역시 사용하지
 																							// 않습니다.
 				.and().authorizeRequests() // 요청에 대한 사용권한 체크
@@ -52,5 +56,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
 				"/configuration/security", "/swagger-ui.html", "/webjars/**");
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfig() {
+
+
+
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowCredentials(true);
+		config.addAllowedOriginPattern("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return source;
 	}
 }
