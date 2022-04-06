d6491ce1e47e652aa925a2b2c6ab4c3db7b24929
minjunglee <3sally@naver.com>
2022-04-07 01:38:52 +0900

    :refresh: Refactor : Modify Freeboard entity [Delete price column
    Former-commit-id: bff87eaaab7b6789eeec38119821ba162b0176cf

--- backend/src/main/java/com/web/wam/model/entity/freeboard/FreeBoard.java
+++ backend/src/main/java/com/web/wam/model/entity/freeboard/FreeBoard.java
@@ -1,9 +1,21 @@
 package com.web.wam.model.entity.freeboard;
 
-import lombok.*;
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @ToString
 @Getter
@@ -14,33 +26,30 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "free_board")
 public class FreeBoard {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "article_id", nullable = false)
-    private Integer articleId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "article_id", nullable = false)
+	private Integer articleId;
 
-    @Column(name = "member_id", nullable = false)
-    private Integer memberId;
+	@Column(name = "member_id", nullable = false)
+	private Integer memberId;
 
-    @Column(name = "title", nullable = false)
-    private String title;
+	@Column(name = "title", nullable = false)
+	private String title;
 
-    @Lob
-    @Column(name = "content", nullable = false)
-    private String content;
+	@Lob
+	@Column(name = "content", nullable = false)
+	private String content;
 
-    @Lob
-    @Column(name = "photo")
-    private String photo;
+	@Lob
+	@Column(name = "photo")
+	private String photo;
 
-    @Lob
-    @Column(name = "tag")
-    private String tag;
+	@Lob
+	@Column(name = "tag")
+	private String tag;
 
-    @Column(name = "price", nullable = false)
-    private Integer price;
-
-    @Column(name = "regtime", nullable = false)
-    private LocalDateTime regtime;
+	@Column(name = "regtime", nullable = false)
+	private LocalDateTime regtime;
 
 }
