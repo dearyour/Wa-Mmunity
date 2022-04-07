c34aa7b7ee2d6c28f5f87c21715b3e544e2476a9
kimms4142 <kimms4142@naver.com>
2022-04-07 17:29:29 +0900

    :fire: Feat : 마이페이지
    Former-commit-id: 47e05d6bb1508039c24d5e88f2321e027d1682e9

---
+++ frontend/components/member/WishList.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import Typography from "@mui/material/Typography";
+const WishListPage = () => {
+  return (
+    <>
+      <Typography variant="h3" align="center">위시리스트</Typography>
+    </>
+  );
+};
+
+export default WishListPage;
